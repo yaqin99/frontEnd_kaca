a2a77c6c02d06d4f79592439e2b1b9eb9eb44e6a
UmarMansyur <70986579+UmarMansyur@users.noreply.github.com>
2021-10-16 06:08:25 +0700

    AutoComplete -> Version 1.1

--- src/components/TranksaksiBio.vue
+++ src/components/TranksaksiBio.vue
@@ -3,12 +3,7 @@
     <div class="col-12 row mt-3">
       <label class="col-sm-3 col-form-label fw-bold">Nama</label>
       <div class="col-sm-9 mt-1">
-        <div class="input-group input-group-sm" >
-          <button class="input-group-text">
-            <span class="fas fa-search"></span>
-          </button>
-          <input   type="text" class="form-control" placeholder="Username">
-        </div>                               
+         <AutoComplete :list="options" item-label="nama" v-model="value" class="form-control form-control-sm" @search="getList" placeholder="Username"></AutoComplete>                          
       </div>
       <label for="hp" class="col-sm-3 col-form-label fw-bold">HP</label>
       <div class="col-sm-9 mt-1">
@@ -22,10 +17,31 @@
       </div>
     </div>
   </div>
+  <div class="col-12  d-sm-flex justify-content-sm-end mt-2">
+    <router-link to="/" class="btn btn-info text-white">
+      <i class="fas fa-history"></i>                                 
+    </router-link>                           
+  </div>
 </template>
 
 <script setup lang="ts">
+  import {ref} from 'vue';
+  import AutoComplete from '../components/AutoComplete.vue';
+  let options = ref([]);
 
+  const value = ref<{ nama:string}>({
+    nama:''
+  });
+
+  const getList = async (e: any) => {
+    const r = await fetch(`http://localhost:8181/pembeli?nama=${e}`);
+    const d = await r.json();
+    options.value = d.map((v: any) => {
+      return {nama:v.nama}
+    });
+    console.log(d)
+    console.log(options.value)
+  }
 </script>
 
 <style>
